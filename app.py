892f2dd1133ccabd4662e2529fbb993cde7d3fe8
Faer-faer <ruslan020507@gmail.com>
2024-01-20 02:23:59 +0300

    commit

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
 from flask import Flask, render_template, request, url_for, redirect
-'''
+
 import mysql.connector
 from werkzeug.security import generate_password_hash, check_password_hash
-'''
+
 app = Flask(__name__)
-'''sql = mysql.connector.connect(
+sql = mysql.connector.connect(
     host="localhost",
     user="root",
     passwd="121377Rr.",
     database='users'
-)'''
+)
 app.config['SECRET_KEY'] = 'Rauf_Russian_People'
 
-# cursor = sql.cursor()
+cursor = sql.cursor()
 
 
 @app.get("/")
 def home():
     return render_template("home.html")
 
-'''
+
 @app.route("/SignUp", methods=["GET", "POST"])
 def SignUp():
     message = ""
@@ -39,7 +39,7 @@ def SignUp():
             sql.commit()
             return redirect(url_for("home"))
     return render_template("signup.html")
-'''
+
 
 if __name__ == '__main__':
     app.run(debug=True)

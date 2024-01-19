a92c27b26b845ca87b324f316e61edbb66b3fbeb
Faer-faer <ruslan020507@gmail.com>
2024-01-20 01:43:49 +0300

    commit
    create users.sql

---
+++ users.sql
@@ -0,0 +1,12 @@
+create table users
+(
+    id         int auto_increment
+        primary key,
+    email      varchar(45)  not null,
+    hash_psswd varchar(200) not null,
+    constraint email
+        unique (email)
+);
+
+INSERT INTO users.users (id, email, hash_psswd) VALUES (3, 'ruslan020507@gmail.com', 'scrypt:32768:8:1$B7W75Lm2liPgRpuf$858f288b6cf6afedf4d829cc9e8f9f0a623210353c4e765b223bac59948a72f66d13708e92dc5c5c88f9f123f1bdf6c3b659cee3cc83fe481f71e21c5c857e77');
+INSERT INTO users.users (id, email, hash_psswd) VALUES (4, 'rusik1486@gmail.com', 'scrypt:32768:8:1$GRZ9ikWWrm1Jd8lZ$8b076f21937cea315612cf0b7a8bc0d97ad631a359e364dc4a94c541d51d918221bea6960e0ee843275876c20ff783203a578443227245d7f54f803d84dd89a2');

043e49bf0639e5a468199164eefd41c0d8903fe3
Arteedser <Arteed.shar@gmail.com>
2024-01-20 02:47:16 +0300

    bd

--- users.sql
+++ users.sql
@@ -6,10 +6,11 @@ USE users;
 DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users;
 (
-    id INT auto_increment PRIMARY KEY,
-    email VARCHAR(45) NOT NULL,
-    hash_psswd VARCHAR(200) NOT NULL,
-    CONSTRAINT email
+    id int NOT NULL AUTO_INCREMENT,
+    email varchar(45) NOT NULL,
+    hash_psswd varchar(200) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY email (`email`)
 );
 
 INSERT INTO users.users (id, email, hash_psswd) VALUES (3, 'ruslan020507@gmail.com', 'scrypt:32768:8:1$B7W75Lm2liPgRpuf$858f288b6cf6afedf4d829cc9e8f9f0a623210353c4e765b223bac59948a72f66d13708e92dc5c5c88f9f123f1bdf6c3b659cee3cc83fe481f71e21c5c857e77');
